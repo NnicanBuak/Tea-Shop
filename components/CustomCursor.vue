f7a01d3ae5d309af2f4d7152195993684455cacf
NnicanBuak <mybailengdb@gmail.com>
2023-04-03 19:21:20 +0800

    update

---
+++ components/CustomCursor.vue
@@ -0,0 +1,47 @@
+<template>
+	<div
+		class="z-20 absolute inset-[-100%] h-6 w-6 rounded-full pointer-events-none"
+		id="cursor"
+		ref="cursor"
+	></div>
+</template>
+
+<script>
+	export default {
+		mounted() {
+			if ('onmousemove' in window) {
+				this.useCustomCursor(this.$refs.cursor)
+			}
+		},
+		methods: {
+			useCustomCursor(cursor) {
+				const offsetX = cursor.offsetWidth / 2
+				const offsetY = cursor.offsetHeight / 2
+				window.addEventListener('mousemove', (e) => {
+					let target = e.target
+					while (target && !target.closest('[data-pointer-type="highlight"]')) {
+						target = target.parentElement
+					}
+					if (target) {
+						cursor.classList.remove('z-20')
+						cursor.classList.add('-z-10')
+
+						const rect = target.getBoundingClientRect()
+						const x = rect.left + rect.width / 2 - offsetX
+						const y = rect.top + rect.height / 2 + window.scrollY - offsetY
+						cursor.style.transform = `translate3d(${x}px, ${y}px, 0)`
+					} else {
+						cursor.classList.remove('-z-10')
+						cursor.classList.add('z-20')
+
+						const x = e.clientX - offsetX
+						const y = e.clientY + window.scrollY - offsetY
+						cursor.style.transform = `translate3d(${x}px, ${y}px, 0)`
+					}
+				})
+
+				cursor.classList.remove('inset-[-100%]')
+			},
+		},
+	}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,5 @@
 <template>
-	<div
-		class="z-20 absolute inset-[-100%] h-6 w-6 rounded-full pointer-events-none"
-		id="cursor"
-		ref="cursor"
-	></div>
+	<CustomCursor />
 	<DesktopHeader
 		v-if="hasHeader && useMediaQuery('(min-width: 1024px)') === true"
 	></DesktopHeader>
@@ -17,8 +13,8 @@
 	</main>
 	<DesktopFooter
 		v-if="hasFooter && useMediaQuery('(min-width: 1024px)') === true"
-	></DesktopFooter>
-	<MobileFooter v-else-if="hasFooter"></MobileFooter>
+	/>
+	<MobileFooter v-else-if="hasFooter" />
 </template>
 
 <script>
@@ -32,41 +28,6 @@
 				hasFooter,
 				hasHeader,
 			}
-		},
-		mounted() {
-			if ('onmousemove' in window) {
-				this.useCustomCursor(this.$refs.cursor)
-			}
-		},
-		methods: {
-			useCustomCursor(cursor) {
-				const offsetX = cursor.offsetWidth / 2
-				const offsetY = cursor.offsetHeight / 2
-				window.addEventListener('mousemove', (e) => {
-					let target = e.target
-					while (target && !target.closest('[data-pointer-type="highlight"]')) {
-						target = target.parentElement
-					}
-					if (target) {
-						cursor.classList.remove('z-20')
-						cursor.classList.add('-z-10')
-
-						const rect = target.getBoundingClientRect()
-						const x = rect.left + rect.width / 2 - offsetX
-						const y = rect.top + rect.height / 2 + window.scrollY - offsetY
-						cursor.style.transform = `translate3d(${x}px, ${y}px, 0)`
-					} else {
-						cursor.classList.remove('-z-10')
-						cursor.classList.add('z-20')
-
-						const x = e.clientX - offsetX
-						const y = e.clientY + window.scrollY - offsetY
-						cursor.style.transform = `translate3d(${x}px, ${y}px, 0)`
-					}
-				})
-
-				cursor.classList.remove('inset-[-100%]')
-			},
 		},
 	}
 </script>
